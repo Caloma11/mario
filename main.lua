59343eefbdf7a280e4ffd04a8898998957266965
gianluca <perronegian@hotmail.com>
2020-04-08 15:39:58 +0800

    initial commit - boilerplate

---
+++ main.lua
@@ -0,0 +1,24 @@
+WINDOW_WIDTH = 1024
+WINDOW_HEIGHT = 576
+
+-- 16:9 res
+
+VIRTUAL_WIDTH = 426
+VIRTUAL_HEIGHT = 240
+
+
+Class = require 'class' -- https://github.com/vrld/hump/blob/master/class.lua
+push = require 'push' -- https://github.com/Ulydev/push
+
+
+function love.load()
+
+end
+
+function love.update()
+
+end
+
+function love.draw()
+
+end
